0c6ffc92ce73d22e40c1adcda19c1d7fc2740a5b
Nicolas Musset <musset.nicolas@gmail.com>
2019-07-14 14:54:37 +0900

    [Shell] Add main menu

---
+++ Avalonia.GameStudio/Avalonia.GameStudio.Shell/ViewModels/MainMenuViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+using Avalonia.GameStudio.Presentation.ViewModels;
+using ReactiveUI;
+
+namespace Avalonia.GameStudio.Shell.ViewModels
+{
+    internal sealed class MainMenuViewModel : ViewModelBase
+    {
+        public MainMenuViewModel()
+        {
+            AboutCommand = ReactiveCommand.Create(AboutCommandImpl);
+            ExitCommand = ReactiveCommand.Create(ExitCommandImpl);
+        }
+
+        public ICommand AboutCommand { get; }
+        public ICommand ExitCommand { get; }
+
+        private void AboutCommandImpl()
+        {
+            Console.WriteLine("Avalonia GameStudio 0.1!");
+            Console.WriteLine("========================");
+            Console.WriteLine("Copyright 2020 - Nicolas Musset");
+        }
+
+        private void ExitCommandImpl()
+        {
+            var lifetime = Application.Current.ApplicationLifetime;
+            if (lifetime is Avalonia.Controls.ApplicationLifetimes.IControlledApplicationLifetime controlled)
+            {
+                controlled.Shutdown();
+                return;
+            }
+
+            Environment.Exit(0);
+        }
+    }
+}
