8dc11e9cacba2dc291a5049445b0008241e76f58
Nicolas Musset <musset.nicolas@gmail.com>
2019-07-14 13:57:47 +0900

    [Presentation] Add convenient method to set the value and notify change

--- Avalonia.GameStudio/Avalonia.GameStudio.Presentation/ViewModels/ViewModelBase.cs
+++ Avalonia.GameStudio/Avalonia.GameStudio.Presentation/ViewModels/ViewModelBase.cs
@@ -7,5 +7,18 @@ namespace Avalonia.GameStudio.Presentation.ViewModels
 {
     public class ViewModelBase : ReactiveObject
     {
+        public bool SetValue<TValue>(ref TValue backingField, TValue value, [CallerMemberName] string propertyName = null)
+        {
+            Contract.Requires<ArgumentNullException>(propertyName != null);
+
+            if (EqualityComparer<TValue>.Default.Equals(backingField, value))
+                return false;
+
+            this.RaisePropertyChanging(propertyName);
+            backingField = value;
+            this.RaisePropertyChanged(propertyName);
+
+            return true;
+        }
     }
 }
